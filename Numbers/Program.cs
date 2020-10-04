2dc93565de3db9576d58a66f8eea4d72411969d1
wildcardjoker <wildcardjoker@users.noreply.github.com>
2020-10-04 10:15:29 +0800

    Add random number generator

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -11,6 +11,29 @@ namespace Numbers
         static void Main(string[] args)
         {
             Console.WriteLine("Shall we play a game?");
+            Console.WriteLine("I'm thinking of a number between 1 and 100. Try and guess it!");
+            Console.WriteLine();
+            var guess = 0;
+            var random = new Random();
+            var correctGuess = random.Next(1, 101);
+
+            while(guess != correctGuess)
+            {
+                Console.Write("What's your guess? ");
+                int.TryParse(Console.ReadLine(), out guess);
+                if (guess != correctGuess)
+                {
+                    if (guess < correctGuess)
+                    {
+                        Console.WriteLine("Higher...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Lower...");
+                    }
+                }
+            }
+            Console.WriteLine($"You win! The correct number was {correctGuess}");
             Console.ReadKey();
         }
     }
