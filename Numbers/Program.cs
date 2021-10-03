7193b70484284d5fbfddd33ed9ea708155011051
wildcardjoker <wildcardjoker@users.noreply.github.com>
2021-10-03 17:28:20 +0800

    Add min/max range when guessing.

--- Numbers/Program.cs
+++ Numbers/Program.cs
@@ -19,9 +19,13 @@ namespace Numbers
             var guessCount = 0;
             var countMessage = "It took you {0} guesses to win. Great game!";
             var correctMessage = "The correct number was {0}.";
+            var rangeMessage = "You should guess a number between {0} and {1}";
+            var min = 1;
+            var max = 100;
 
             while (guess != correctGuess)
             {
+                Console.WriteLineFormatted(rangeMessage, Color.Fuchsia, Color.Gainsboro, min, max);
                 Console.Write("What's your guess? ");
                 int.TryParse(Console.ReadLine(), out guess);
                 guessCount++;
@@ -35,15 +39,19 @@ namespace Numbers
                 if (guess != correctGuess)
                     Console.WriteLine(guess < correctGuess ? "Higher..." : "Lower...",
                         guess < correctGuess ? Color.Gold : Color.LightSkyBlue);
+                if (guess<correctGuess)
+                {
+                    min = guess;
+                }
+                else
+                {
+                    max = guess;
+                }
             }
 
             Console.WriteAscii($"{correctGuess} - You win!", Color.SpringGreen);
             Console.WriteLineFormatted(correctMessage, Color.CornflowerBlue, Color.LawnGreen, correctGuess);
             Console.WriteLineFormatted(countMessage, Color.Aqua, Color.Gray, guessCount);
-            Console.ResetColor();
-
-            Console.Write("Press the any key to exit :)");
-            Console.ReadKey();
         }
     }
 }
